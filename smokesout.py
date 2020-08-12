1e1389e63f21eba9373f378259a00e9af299060d
bloos3rpent <67107159+bloos3rpent@users.noreply.github.com>
2020-08-12 23:16:10 +0800

    Minor Bug Fixes

--- smokesout.py
+++ smokesout.py
@@ -33,14 +33,14 @@ def clear():
         _ = system('clear')
 def main():
     print(f"""{bcolors.FAIL}
-
- ______  __    __  ______  __  __  ______  ______  ______  __  __  ______  
-/\  ___\/\ "-./  \/\  __ \/\ \/ / /\  ___\/\  ___\/\  __ \/\ \/\ \/\__  _\ 
-\ \___  \ \ \-./\ \ \ \/\ \ \  _"-\ \  __ \\ \___  \ \ \/\ \ \ \_\ \/_/\ \/ 
- \/\_____\ \_\ \ \_\ \_____\ \_\ \_\ \_____\/\_____\ \_____\ \_____\ \ \_\ 
-  \/_____/\/_/  \/_/\/_____/\/_/\/_/\/_____/\/_____/\/_____/\/_____/  \/_/                                                                     
+   _____                 _                         _   
+  / ____|               | |                       | |  
+ | (___  _ __ ___   ___ | | _____  ___  ___  _   _| |_ 
+  \___ \| '_ ` _ \ / _ \| |/ / _ \/ __|/ _ \| | | | __|
+  ____) | | | | | | (_) |   <  __/\__ \ (_) | |_| | |_ 
+ |_____/|_| |_| |_|\___/|_|\_\___||___/\___/ \__,_|\__|                                                                 
      {bcolors.ENDC}""")  # logo
-    print("        Beta Version        |       Developed by bloos3rpent")
+    print("   Beta Version   |   Developed by bloos3rpent")
     print(f"""{bcolors.OKBLUE}
     [1] Attack Website
     [2] Check Website Status
@@ -102,6 +102,8 @@ def main():
     def encrypt():
         clear()
         print(f"""{bcolors.OKBLUE}
+----Decrypt Code----
+
     [1] MD5 Hash
     [2] SHA-256
     [3] 0xHex
@@ -115,7 +117,7 @@ def main():
             if cipher == "1": #MD5 HASH
                 string = input(f"{bcolors.OKGREEN}Enter String to Hash > {bcolors.ENDC}")
                 hash_obj = hashlib.md5(string.encode())
-                print("MD5 Encrypted > " + hash_obj.hexdigest())
+                print(f"{bcolors.FAIL}MD5 Encrypted > {bcolors.OKGREEN}%s{bcolors.ENDC}" % (hash_obj.hexdigest()))
                 a = input("Encrypt another? (y/n) > ")
                 if a.lower() == "y":
                     encrypt()
@@ -126,7 +128,7 @@ def main():
             elif cipher == "2": #SHA-256
                 string = input(f"{bcolors.OKGREEN}Enter String to Hash > {bcolors.ENDC}")
                 hash_obj = hashlib.sha256(string.encode())
-                print("SHA-256 Encrypted > " + hash_obj.hexdigest())
+                print(f"{bcolors.FAIL}SHA-256 Encrypted > {bcolors.OKGREEN}%s{bcolors.ENDC}" % (hash_obj.hexdigest()))
                 a = input("Encrypt another? (y/n) > ")
                 if a.lower() == "y":
                     encrypt()
@@ -137,7 +139,7 @@ def main():
             elif cipher == "3":
                 string = input(f"{bcolors.OKGREEN}Enter String to Encode > {bcolors.ENDC}")
                 s = string.encode('utf-8')
-                print("0xHex Encoded > " + "0x" + s.hex())
+                print(f"{bcolors.FAIL}0xHex Encoded > {bcolors.OKGREEN}0x%s{bcolors.ENDC}" % (s.hex()))
                 a = input("Encrypt another? (y/n) > ")
                 if a.lower() == "y":
                     encrypt()
@@ -146,13 +148,14 @@ def main():
                     main()
                 encrypt()
             elif cipher == "4":
+
                 string = input(f"{bcolors.OKGREEN}Enter String to Encode > {bcolors.ENDC}")
                 byte_array = string.encode()
 
                 binary_int = int.from_bytes(byte_array, "big")
                 binary_string = bin(binary_int)
 
-                print(binary_string)
+                print(f"{bcolors.FAIL}0bBinary Encoded > {bcolors.OKGREEN}%s{bcolors.ENDC}" % (binary_string))
                 a = input("Encrypt another? (y/n) > ")
                 if a.lower() == "y":
                     encrypt()
@@ -166,7 +169,7 @@ def main():
                 base64_bytes = base64.b64encode(message_bytes)
                 base64_message = base64_bytes.decode('ascii')
 
-                print("Base64 Encoded > " + base64_message)
+                print(f"{bcolors.FAIL}Base64 Encoded > {bcolors.OKGREEN}%s{bcolors.ENDC}" % (base64_message))
                 a = input("Encrypt another? (y/n) > ")
                 if a.lower() == "y":
                     encrypt()
@@ -184,7 +187,8 @@ def main():
     def decode():
         clear()
         print(f"""{bcolors.OKBLUE}
-
+----Decrypt Code----
+    
     [1] Decrypt Hash
     [2] 0xHex  Decode
     [3] 0bBinary Decode
@@ -192,80 +196,95 @@ def main():
     
     {bcolors.FAIL}[99] Back
         {bcolors.ENDC}""")
-        d_option = input("Encryption to Decrypt/Decode > ")
-        if d_option == "1":
-            clear()
-            print(f"""{bcolors.OKBLUE}
+        def d_options():
+            d_option = input("Encryption to Decrypt/Decode > ")
+            if d_option == "1":
+                clear()
+                print(f"""{bcolors.OKBLUE}
+----Hash Reverse Lookup Sites----
+
     [1] crackstation.net {bcolors.OKGREEN}(RECOMMENDED){bcolors.ENDC}{bcolors.OKBLUE}
     [2] md5online.org
     [3] md5decrypt.net
     [4] dcode.fr
     [5] hashes.com
 
-    {bcolors.FAIL}[99] Back
-            {bcolors.ENDC}""")
-            def askd():
-                dec_opt = input("Choose site > ")
-                if dec_opt == "1":
-                    webbrowser.open("crackstation.net")
-                if dec_opt == "2":
-                    webbrowser.open("md5online.org")
-                if dec_opt == "3":
-                    webbrowser.open("md5decrypt.net")
-                if dec_opt == "4":
-                    webbrowser.open("dcode.fr")
-                if dec_opt == "5":
-                    webbrowser.open("hashes.com")
-                if dec_opt == "99":
-                    clear()
+----Redirection to sites don't work in Termux----
+        {bcolors.FAIL}[99] Back
+                {bcolors.ENDC}""")
+                def askd():
+                    dec_opt = input("Choose site > ")
+                    if dec_opt == "1":
+                        webbrowser.open("crackstation.net")
+                    if dec_opt == "2":
+                        webbrowser.open("md5online.org")
+                    if dec_opt == "3":
+                        webbrowser.open("md5decrypt.net")
+                    if dec_opt == "4":
+                        webbrowser.open("dcode.fr")
+                    if dec_opt == "5":
+                        webbrowser.open("hashes.com")
+                    if dec_opt == "99":
+                        clear()
+                        decode()
+                    else:
+                        askd()
+                askd()
+            elif d_option == "2":
+                string = input("Translate from 0xHex Format > ")
+                line = re.sub('[0x]', '', string)
+                hexForm = bytearray.fromhex(line).decode()
+                print(f"{bcolors.FAIL}0xHex Decoded > {bcolors.ENDC}{bcolors.OKGREEN}%s{bcolors.ENDC}" % (hexForm))
+                a = input("Decrypt Another? (y/n) > ")
+                if a.lower() == "y":
                     decode()
                 else:
-                    askd()
-            askd()
-        elif d_option == "2":
-            string = input("Translate from 0xHex Format > ")
-            line = re.sub('[0x]', '', string)
-            hexForm = bytearray.fromhex(line).decode()
-            print("0xHex Decoded > " + hexForm)
+                    clear()
+                    main()
+            elif d_option == "3":
+                try:
+                    string = input("0bBINARY format to decode > ")
+                    binaryform = string[2:]
+                    binary_int = int(binaryform, 2)
+                    byte_number = binary_int.bit_length() + 7 // 8
+
+                    binary_array = binary_int.to_bytes(byte_number, "big")
+                    ascii_text = binary_array.decode()
+
+                    print(f"{bcolors.FAIL}Decoded 0bBinary code > {bcolors.OKGREEN}%s{bcolors.ENDC}" % (ascii_text))
+
+                    a = input("Decrypt Another? (y/n) > ")
+                    if a.lower() == "y":
+                        decode()
+                    else:
+                        clear()
+                        main()
+                except:
+                    print(f"{bcolors.WARNING}Error{bcolors.ENDC}")
+                    time.sleep(2)
+                    clear()
+                    decode()
+            elif d_option == "4":
+                base64_message = input("Base64 code to decode > ")
+                base64_bytes = base64_message.encode('ascii')
+                message_bytes = base64.b64decode(base64_bytes)
+                message = message_bytes.decode('ascii')
+
+                print(f"{bcolors.FAIL}Base64 decoded > {bcolors.OKGREEN}%s{bcolors.ENDC}" % (message))
+            elif d_option == "99":
+                clear()
+                main()
+            else:
+                d_options()
+                main()
             a = input("Decrypt Another? (y/n) > ")
             if a.lower() == "y":
+                clear()
                 decode()
             else:
                 clear()
                 main()
-        elif d_option == "3":
-            string = input("0bBINARY format to decode > ")
-            binaryform = string[2:]
-            binary_int = int(binaryform, 2)
-            byte_number = binary_int.bit_length() + 7 // 8
-
-            binary_array = binary_int.to_bytes(byte_number, "big")
-            ascii_text = binary_array.decode()
-
-            print("Decoded 0bBinary code > " + ascii_text)
-
-            a = input("Decrypt Another? (y/n) > ")
-            if a.lower() == "y":
-                decode()
-            else:
-                clear()
-                main()
-        elif d_option == "4":
-            base64_message = input("Base64 code to decode > ")
-            base64_bytes = base64_message.encode('ascii')
-            message_bytes = base64.b64decode(base64_bytes)
-            message = message_bytes.decode('ascii')
-
-            print("Base64 decoded > " + message)
-        elif d_option == "99":
-            main()
-        a = input("Decrypt Another? (y/n) > ")
-        if a.lower() == "y":
-            clear()
-            decode()
-        else:
-            clear()
-            main()
+        d_options()
 
     def mainask():
         action = input(f"{bcolors.FAIL}Smokesout > {bcolors.ENDC}")
